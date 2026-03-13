e71531fa1607ceb246e90b5c7388623d2923adcd
ROBIN <jollymollyguy@gmail.com>
2026-03-13 20:36:55 +0545

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-def divide(a, b):
-    try:
-        result= a / b
-        return result 
-    expect ZeroDivisionError:
-        return "Error: cannot divide by zero."
-
-print(divide(10, 2))
-print(divide(10, 0))
